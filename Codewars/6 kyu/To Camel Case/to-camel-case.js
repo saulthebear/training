cb351aa6f53c99690751781e097223b8d1897677
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-03-13 21:56:04 +0800

    Fixed To Camel Case

--- Codewars/6 kyu/To Camel Case/to-camel-case.js
+++ Codewars/6 kyu/To Camel Case/to-camel-case.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 function toCamelCase(str) {
-  return str.replace(/([-_])(\w)/g, (delim, c) => {
-    return c.toUpperCase();
+  return str.replace(/([-_])(\w)/g, (match, delim, char) => {
+    return char.toUpperCase();
   });
 }
