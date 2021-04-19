2fb20ed2c6991a436c7ac79becc56dfd68de4c43
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-04-19 14:23:29 +0800

    Added format_name.rb

--- AppAcademy Open/Intro to Programming/10. Enumerables & Ranges/format_name.rb
+++ AppAcademy Open/Intro to Programming/10. Enumerables & Ranges/format_name.rb
@@ -3,7 +3,10 @@
 # "abc".upcase # => "ABC"
 
 def format_name(str)
-
+  words = str.split(' ')
+  new_words = []
+  words.each { |word| new_words << word.capitalize }
+  return new_words.join(' ')
 end
 
 puts format_name("chase WILSON") # => "Chase Wilson"
