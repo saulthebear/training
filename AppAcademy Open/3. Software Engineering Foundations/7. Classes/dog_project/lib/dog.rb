2eee0558c178e1cd495e700c8531a0ce8c4d7dfb
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-01 15:23:17 -0600

    Completed Dog Project

--- AppAcademy Open/3. Software Engineering Foundations/7. Classes/dog_project/lib/dog.rb
+++ AppAcademy Open/3. Software Engineering Foundations/7. Classes/dog_project/lib/dog.rb
@@ -1,2 +1,40 @@
-class Dog
+def initialize(name, breed, age, bark, favorite_foods)
+  @name = name
+  @breed = breed
+  @age = age
+  @bark = bark
+  @favorite_foods = favorite_foods
+end
+
+# Getters
+def name
+  @name
+end
+
+def breed
+  @breed
+end
+
+def age
+  @age
+end
+
+def bark
+  return @bark.upcase if @age > 3
+
+  @bark.downcase
+end
+
+def favorite_foods
+  @favorite_foods
+end
+
+# Setters
+def age=(age)
+  @age = age
+end
+
+# Methods
+def favorite_food?(food)
+  @favorite_foods.map(&:downcase).include?(food.downcase)
 end
