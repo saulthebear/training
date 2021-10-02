b18ddc200b9dcdf8c24b98aea72296c597c50a54
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-02 14:24:31 -0600

    Completed Part 1

--- AppAcademy Open/3. Software Engineering Foundations/10. Object Oriented Programming/battleship_project/lib/board.rb
+++ AppAcademy Open/3. Software Engineering Foundations/10. Object Oriented Programming/battleship_project/lib/board.rb
@@ -1,3 +1,31 @@
 class Board
+  attr_reader :size
+
+  def initialize(n)
+    @size = n * n
+    @grid = []
+    n.times do
+      row = []
+      n.times { row << :N }
+      @grid << row
+    end
+  end
+
+  def [](position)
+    @grid[position[0]][position[1]]
+  end
+
+  def []=(position, value)
+    @grid[position[0]][position[1]] = value
+  end
+
+  def num_ships
+    count = 0
+    @grid.each do |row|
+      count += row.each.count { |val| val == :S }
+    end
+    count
+  end
+
   
 end
