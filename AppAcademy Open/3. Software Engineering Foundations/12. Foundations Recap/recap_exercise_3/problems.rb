e7d8a9e0583e536de032904533b07473c72fa2dd
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-04 14:22:14 -0600

    Completed Part 1 (General Problems)

--- AppAcademy Open/3. Software Engineering Foundations/12. Foundations Recap/recap_exercise_3/problems.rb
+++ AppAcademy Open/3. Software Engineering Foundations/12. Foundations Recap/recap_exercise_3/problems.rb
@@ -1,24 +1,123 @@
-# General problems
+require 'byebug'
 
-def no_dupes?(arr) end
+## Part 1: General problems
 
-def no_consecutive_repeats?(arr) end
+def no_dupes?(arr)
+  ele_counts = Hash.new(0)
 
-def char_indices(str) end
+  arr.each { |ele| ele_counts[ele] += 1 }
 
-def longest_streak(str) end
+  selected = []
 
-def char_indices(str) end
+  ele_counts.each { |ele, count| selected << ele if count == 1 }
+  selected
+end
 
-def longest_streak(str) end
+def no_consecutive_repeats?(arr)
+  (0...arr.length).each do |idx1|
+    idx2 = idx1 + 1
+    return false if arr[idx1] == arr[idx2]
+  end
+  true
+end
 
-def bi_prime?(num) end
+def char_indices(str)
+  indices = Hash.new { |hash, key| hash[key] = [] }
+  str.chars.each.with_index { |char, idx| indices[char] << idx }
+  indices
+end
 
-def vigenere_cipher(message, keys) end
+def longest_streak(str)
+  long_streak = ''
+  curr_streak_char = ''
+  curr_streak_count = 0
 
-def voewl_rotate(str) end
+  str.chars.each do |char|
+    if curr_streak_char == ''
+      curr_streak_char = char
+      curr_streak_count = 1
+    elsif curr_streak_char == char
+      curr_streak_count += 1
+    else
+      curr_streak_char = char
+      curr_streak_count = 1
+    end
 
-# Proc Problems
+    if curr_streak_count >= long_streak.length
+      long_streak = curr_streak_char * curr_streak_count
+    end
+  end
+  long_streak
+end
+
+def prime?(num)
+  (2...num).each do |f|
+    return false if (num % f).zero?
+  end
+  true
+end
+
+def list_primes(max)
+  (2..max).to_a.select { |n| prime?(n) }
+end
+
+def bi_prime?(num)
+  half = (num / 2.0).ceil
+  primes = list_primes(half)
+
+  (0...primes.length).each do |idx1|
+    (idx1...primes.length).each do |idx2|
+      pair_product = primes[idx1] * primes[idx2]
+      return true if pair_product == num
+    end
+  end
+
+  false
+end
+
+def caesar_cipher(message, offset)
+  alpha = ('a'..'z').to_a
+  cipher_arr = []
+
+  message.chars.each do |char|
+    new_char_idx = (alpha.index(char) + offset) % 26
+    cipher_arr << alpha[new_char_idx]
+  end
+  cipher_arr.join
+end
+
+def vigenere_cipher(message, keys)
+  cipher_arr = []
+  message.chars.each.with_index do |char, idx|
+    key_to_use = keys[idx % keys.length]
+    cipher_arr << caesar_cipher(char, key_to_use)
+  end
+  cipher_arr.join
+end
+
+def vowel_rotate(str)
+  vowels = 'aeiou'
+  new_str = ''
+  str.chars.each.with_index do |char, idx|
+    if vowels.include?(char)
+      idx2 = idx - 1
+      prev_vowel_found = false
+      until prev_vowel_found
+        if vowels.include?(str[idx2])
+          new_str += str[idx2]
+          prev_vowel_found = true
+        else
+          idx2 -= 1
+        end
+      end
+    else
+      new_str += char
+    end
+  end
+  new_str
+end
+
+## Part 2: Proc Problems
 
 class String
   def select(&prc) end
@@ -26,7 +125,7 @@ class String
   def map!(&prc) end
 end
 
-# Recursion Problems
+## Part 3: Recursion Problems
 
 def multiply(a, b) end
 
---
+++ AppAcademy Open/3. Software Engineering Foundations/12. Foundations Recap/recap_exercise_3/tester.rb
@@ -0,0 +1,46 @@
+require './problems'
+
+puts 'Testing no_dupes?(arr)'
+p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
+p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
+p no_dupes?([true, true, true])            # => []
+
+puts 'Testing no_consecutive_repeats?(arr)'
+p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+p no_consecutive_repeats?(['x'])                              # => true
+
+puts 'Testing char_indices(str)'
+p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+
+puts 'Testing longest_streak(str)'
+p longest_streak('a')           # => 'a'
+p longest_streak('accccbbb')    # => 'cccc'
+p longest_streak('aaaxyyyyyzz') # => 'yyyyy
+p longest_streak('aaabbb')      # => 'bbb'
+p longest_streak('abc')         # => 'c'
+
+puts 'Testing bi_prime?()'
+p bi_prime?(14)   # => true
+p bi_prime?(22)   # => true
+p bi_prime?(25)   # => true
+p bi_prime?(94)   # => true
+p bi_prime?(24)   # => false
+p bi_prime?(64)   # => false
+
+puts 'Testing vigenere_chipher(message, keys)'
+p vigenere_cipher("toerrishuman", [1])        # => "upfssjtivnbo"
+p vigenere_cipher("toerrishuman", [1, 2])     # => "uqftsktjvobp"
+p vigenere_cipher("toerrishuman", [1, 2, 3])  # => "uqhstltjxncq"
+p vigenere_cipher("zebra", [3, 0])            # => "ceerd"
+p vigenere_cipher("yawn", [5, 1])             # => "dbbo"
+
+puts 'Testing vowel_rotate(str)'
+p vowel_rotate('computer')      # => "cempotur"
+p vowel_rotate('oranges')       # => "erongas"
+p vowel_rotate('headphones')    # => "heedphanos"
+p vowel_rotate('bootcamp')      # => "baotcomp"
+p vowel_rotate('awesome')       # => "ewasemo"
