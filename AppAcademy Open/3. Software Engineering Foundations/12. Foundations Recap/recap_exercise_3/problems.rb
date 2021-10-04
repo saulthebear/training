3bff160eb66348b6b8ee3babd6b38a4f6f0af474
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-04 11:30:37 -0600

    Created Skeleton for Recap Exercises 3

---
+++ AppAcademy Open/3. Software Engineering Foundations/12. Foundations Recap/recap_exercise_3/problems.rb
@@ -0,0 +1,35 @@
+# General problems
+
+def no_dupes?(arr) end
+
+def no_consecutive_repeats?(arr) end
+
+def char_indices(str) end
+
+def longest_streak(str) end
+
+def char_indices(str) end
+
+def longest_streak(str) end
+
+def bi_prime?(num) end
+
+def vigenere_cipher(message, keys) end
+
+def voewl_rotate(str) end
+
+# Proc Problems
+
+class String
+  def select(&prc) end
+
+  def map!(&prc) end
+end
+
+# Recursion Problems
+
+def multiply(a, b) end
+
+def lucas_sequence(length) end
+
+def prime_factorization(num) end
