d1d898ac0f9c9a22bd2d65ec1d3c1c9596f4c901
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-05 21:36:29 -0600

    Added Skeleton for Perilous Procs Exercises

---
+++ AppAcademy Open/3. Software Engineering Foundations/13. Aditional Projects/perilous_procs/perilous-procs.rb
@@ -0,0 +1,41 @@
+## Phase 1
+
+def some?() end
+
+def exactly?() end
+
+def filter_out() end
+
+def at_least?() end
+
+def every?() end
+
+def at_most() end
+
+def first_index() end
+
+
+## Phase 2
+
+def xnor_select() end
+
+def filter_out!() end
+
+def multi_map() end
+
+def proctition() end
+
+
+## Phase 3
+
+def selected_map!() end
+
+def chain_map() end
+
+def proc_suffix() end
+
+def proctition_platinum() end
+
+def procipher() end
+
+def picky_procipher() end
