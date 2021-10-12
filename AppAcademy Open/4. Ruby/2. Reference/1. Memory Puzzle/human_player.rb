c8b4f917ee11afcd7d33ae739206dcc0d314860a
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-12 17:59:51 -0600

    Invalid guesses handled

--- AppAcademy Open/4. Ruby/2. Reference/1. Memory Puzzle/human_player.rb
+++ AppAcademy Open/4. Ruby/2. Reference/1. Memory Puzzle/human_player.rb
@@ -6,11 +6,20 @@ class HumanPlayer
 
   def get_position
     row, col = get_input.delete(' ').split(',').map(&:to_i)
-    Position.new(row, col)
+    position = nil
+    begin
+      position = Position.new(row, col)
+    rescue ArgumentError => e
+      puts e.message
+    end
+    position
   end
 
   def prompt(message)
     puts message
   end
-  
+
+  def receive_revealed_card(position, value) end
+
+  def receive_match(position1, position2) end
 end
--- AppAcademy Open/4. Ruby/2. Reference/1. Memory Puzzle/position.rb
+++ AppAcademy Open/4. Ruby/2. Reference/1. Memory Puzzle/position.rb
@@ -3,7 +3,7 @@ class Position
   attr_reader :row, :col
 
   def initialize(row, col)
-    raise 'Position values must be integers' unless row.is_a?(Integer) && col.is_a?(Integer)
+    raise ArgumentError, 'Position values must be integers' unless row.is_a?(Integer) && col.is_a?(Integer)
 
     @row = row
     @col = col
@@ -18,5 +18,21 @@ class Position
     end
 
     true
+  end
+
+  def hash
+    [@row, @col].hash
+  end
+
+  def eql?(other)
+    @row == other.row && @col == other.col
+  end
+
+  def ==(other)
+    @row == other.row && @col == other.col
+  end
+
+  def inspect
+    "#<Position: #{object_id} @row=#{@row}, @col=#{@col}>"
   end
 end
