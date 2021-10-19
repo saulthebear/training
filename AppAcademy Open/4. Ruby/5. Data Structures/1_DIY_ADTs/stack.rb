cc71d8a80f0ba1f3e4f777f0f924f6fe6171b416
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-19 17:36:16 -0600

    Implement Stack

--- AppAcademy Open/4. Ruby/5. Data Structures/1_DIY_ADTs/stack.rb
+++ AppAcademy Open/4. Ruby/5. Data Structures/1_DIY_ADTs/stack.rb
@@ -1,17 +1,17 @@
 class Stack
   def initialize
-    # create ivar to store stack here!
+    @data = []
   end
 
   def push(el)
-    # adds an element to the stack
+    @data << el
   end
 
   def pop
-    # removes one element from the stack
+    @data.pop
   end
 
   def peek
-    # returns, but doesn't remove, the top element in the stack
+    @data[-1]
   end
 end
