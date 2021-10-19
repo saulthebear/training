1876fec964bee11f8d92a1a0cfb4876c687b9932
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-19 17:39:47 -0600

    Implement Queue

--- AppAcademy Open/4. Ruby/5. Data Structures/1_DIY_ADTs/queue.rb
+++ AppAcademy Open/4. Ruby/5. Data Structures/1_DIY_ADTs/queue.rb
@@ -1,14 +1,17 @@
 class Queue
   def initialize
-    
+    @data = []
   end
 
   def enqueue(el)
+    @data << el
   end
 
   def dequeue
+    @data.shift
   end
 
   def peek
+    @data[0]
   end
 end
