cdef7726c7562eb1581714277b0515e2d87ef283
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-19 17:42:33 -0600

    Add Map skeleton

---
+++ AppAcademy Open/4. Ruby/5. Data Structures/1_DIY_ADTs/map.rb
@@ -0,0 +1,17 @@
+# Implement a map using a 2D array
+class Map
+  def initialize
+  end
+
+  def set(key, value)
+  end
+
+  def get(key)
+  end
+
+  def delete(key)
+  end
+  
+  def show
+  end
+end
