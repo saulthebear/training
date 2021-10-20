22f9dc802da16bd2f0c4c839854e1898185d298f
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-20 14:04:54 -0600

    Implement PolyTreeNode

--- AppAcademy Open/4. Ruby/5. Data Structures/2_poly_tree_node/lib/00_tree_node.rb
+++ AppAcademy Open/4. Ruby/5. Data Structures/2_poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,35 @@
+require 'byebug'
 class PolyTreeNode
+  attr_reader :value, :parent, :children
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    prev_parent = @parent
+    prev_parent&.children&.delete(self)
+
+    @parent = node
+
+    return if node.nil? || node.children.include?(self)
+
+    node.children << self
+  end
+
+  def add_child(node)
+    node.parent = self
+  end
+
+  def remove_child(node)
+    raise "#{node} is not a child!" unless @children.include?(node)
+    node.parent = nil
+  end
+
+  def inspect
+    # "<PolyTreeNode @value=#{@value} @parent=#{@parent.inspect} @children=#{@children.inspect}>"
+    "<PolyTreeNode @value=#{@value} @parent=#{@parent.inspect}>"
+  end
 end
