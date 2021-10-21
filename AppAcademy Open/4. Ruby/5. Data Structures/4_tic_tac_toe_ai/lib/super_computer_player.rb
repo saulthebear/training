485531bad72a382c8ff5b79bf71dedba12d9634f
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-20 20:28:39 -0600

    Implement SuperComputerPlayer

--- AppAcademy Open/4. Ruby/5. Data Structures/4_tic_tac_toe_ai/lib/super_computer_player.rb
+++ AppAcademy Open/4. Ruby/5. Data Structures/4_tic_tac_toe_ai/lib/super_computer_player.rb
@@ -2,6 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    children = node.children
+    winners = children.select { |child| child.winning_node?(mark) }
+
+    return winners[0].prev_move_pos unless winners.empty?
+
+    non_losers = children.reject { |child| child.losing_node?(mark) }
+    raise 'Unable to find move' if non_losers.empty?
+
+    non_losers[0].prev_move_pos
   end
 end
 
