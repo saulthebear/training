775925da85af25f45bac6ae0bc58971e59b46c51
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-23 15:21:32 -0600

    Reorganize

--- AppAcademy Open/4. Ruby/6. OOP/7. Chess/piece.rb
+++
@@ -1,6 +0,0 @@
-# Holds functionality common to all chess pieces
-class Piece
-  def initialize
-    
-  end
-end
---
+++ AppAcademy Open/4. Ruby/6. OOP/7. Chess/pieces/slideable.rb
@@ -0,0 +1,3 @@
+# Mixin for pieces that move diagonally or vertically/horizontally
+module Slideable
+end
