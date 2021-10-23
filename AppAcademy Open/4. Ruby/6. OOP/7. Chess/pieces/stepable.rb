e37df1ae8d41dba1765c55f09bc2a1eaa323e2c7
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-23 16:47:30 -0600

    Fix stepable not checking move validity

--- AppAcademy Open/4. Ruby/6. OOP/7. Chess/pieces/stepable.rb
+++ AppAcademy Open/4. Ruby/6. OOP/7. Chess/pieces/stepable.rb
@@ -8,12 +8,32 @@ module Stepable
       dy = diff[0]
       curr_pos = @pos.dup
       new_pos = [curr_pos[0] + dx, curr_pos[1] + dy]
-      moves << new_pos if Board.valid_position?(new_pos)
+      
+      moves << new_pos if can_move_here(new_pos)
     end
     moves
   end
 
   private
+
+  def can_move_here(pos)
+    return false unless pos_in_range(pos)
+    return false unless pos_empty?(pos) || pos_takeable(pos)
+    
+    true
+  end
+
+  def pos_in_range(pos)
+    Board.valid_position?(pos)
+  end
+
+  def pos_empty?(pos)
+    @board[pos] == NullPiece
+  end
+
+  def pos_takeable(pos)
+    @board[pos].color != @color
+  end
 
   # Overwritten by subclass
   # Returns an array of [dx, dy] position changes
