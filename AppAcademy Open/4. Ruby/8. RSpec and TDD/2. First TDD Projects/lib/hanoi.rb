a94d0243c57fb4f04b65bd81051abb0bfcf3b054
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-28 13:16:21 -0600

    Write tests and implement Towers of Hanoi

--- AppAcademy Open/4. Ruby/8. RSpec and TDD/2. First TDD Projects/lib/hanoi.rb
+++ AppAcademy Open/4. Ruby/8. RSpec and TDD/2. First TDD Projects/lib/hanoi.rb
@@ -1,3 +1,45 @@
+# require 'byebug'
 class Hanoi
+  attr_reader :pegs
 
+  def initialize
+    @pegs = Array.new(3) { [] }
+    @pegs[0] = [5, 4, 3, 2, 1]
+  end
+
+  def move(start_peg, end_peg)
+    raise MoveError, 'That peg is empty! Nothing to move' if @pegs[start_peg].empty?
+
+    unless @pegs[end_peg].empty?
+      disk_to_move_size = @pegs[start_peg][-1]
+      target_size = @pegs[end_peg][-1]
+      err_msg = "#{disk_to_move_size} cannot fit ontop of #{target_size}"
+      raise MoveError, err_msg if disk_to_move_size > target_size
+    end
+    disk = @pegs[start_peg].pop
+    @pegs[end_peg] << disk
+
+    render
+  end
+
+  def won?
+    @pegs[1..2].any? { |peg| peg.length == 5 }
+  end
+
+  def render
+    puts '-----'
+    pegs_dup = []
+    @pegs.each { |peg| pegs_dup << peg.dup }
+
+    pegs_dup.each { |peg| peg << ' ' until peg.length == 5 }
+
+    i = 4
+    until i.negative?
+      puts "#{pegs_dup[0][i]} #{pegs_dup[1][i]} #{pegs_dup[2][i]}"
+      i -= 1
+    end
+  end
+end
+
+class MoveError < ArgumentError
 end
--- AppAcademy Open/4. Ruby/8. RSpec and TDD/2. First TDD Projects/spec/hanoi_spec.rb
+++ AppAcademy Open/4. Ruby/8. RSpec and TDD/2. First TDD Projects/spec/hanoi_spec.rb
@@ -2,23 +2,96 @@ require 'rspec'
 require 'hanoi'
 
 describe Hanoi do
-
+  subject { Hanoi.new }
   describe '#initialize' do
-    it 'starts with 3 empty pegs'
+    it 'starts with 5 disks on one peg, and two empty pegs' do
+      expect(subject.pegs).to contain_exactly([], [], [5, 4, 3, 2, 1])
+    end
   end
 
   describe '#move' do
-    it 'allows moving a small disk onto a larger one'
+    it 'moves the top peg from one stack to another' do
+      subject.move(0, 1)
+      expect(subject.pegs).to eq([[5, 4, 3, 2], [1], []]) 
+      subject.move(0, 2)
+      expect(subject.pegs).to eq([[5, 4, 3], [1], [2]]) 
 
-    it 'doesn\'t allow moving a larger disk onto a smaller disk'
+    end
 
-    it 'allows moving any disk onto an empty peg'
+    it 'allows moving a small disk onto a larger one' do
+      subject.move(0, 1)
+      subject.move(0, 2)
+      subject.move(1, 2)
+      expect(subject.pegs).to eq([[5, 4, 3], [], [2, 1]]) 
+    end
+
+    it 'doesn\'t allow moving a larger disk onto a smaller disk' do
+      subject.move(0, 1)
+      expect { subject.move(0, 1) }.to raise_error(MoveError, '2 cannot fit ontop of 1')
+    end
+
+    it 'raises an error when trying to move from an empty peg' do
+      err_msg = 'That peg is empty! Nothing to move'
+      expect { subject.move(1, 2) }.to raise_error(MoveError, err_msg)
+    end
   end
 
   describe '#won?' do
-    it 'returns false unless all disks are on one peg'
+    before(:each) do
+      subject.move(0, 2)
+      subject.move(0, 1)
+      subject.move(2, 1)
+      subject.move(0, 2)
+      subject.move(1, 0)
 
-    it 'returns true if all disks are on one peg'
+      subject.move(1, 2)
+      subject.move(0, 2)
+      subject.move(0, 1)
+      subject.move(2, 1)
+      subject.move(2, 0)
 
+      subject.move(1, 0)
+      subject.move(2, 1)
+      subject.move(0, 2)
+      subject.move(0, 1)
+      subject.move(2, 1)
+
+      subject.move(0, 2)
+      subject.move(1, 0)
+      subject.move(1, 2)
+      subject.move(0, 1)
+      subject.move(1, 2)
+
+      subject.move(1, 0)
+      subject.move(2, 1)
+      subject.move(2, 0)
+      subject.move(1, 0)
+      subject.move(1, 2)
+
+      subject.move(0, 2)
+      subject.move(0, 1)
+      subject.move(2, 1)
+      subject.move(0, 2)
+      subject.move(1, 0)
+
+      subject.move(1, 2)
+    end
+
+    context 'In starting position' do
+      in_start_position = Hanoi.new
+      it 'doesn\'t return true' do
+        expect(in_start_position.pegs).to eq([[5, 4, 3, 2, 1], [], []])
+        expect(in_start_position).to_not be_won
+      end
+    end
+
+    it 'returns false unless all disks are on one peg' do
+      expect(subject).to_not be_won
+    end
+
+    it 'returns true if all disks are on one peg' do
+      subject.move(0, 2)
+      expect(subject).to be_won
+    end
   end
 end
