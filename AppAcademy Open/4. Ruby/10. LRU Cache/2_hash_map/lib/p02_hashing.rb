046e4c8ec0753ae53b913c7950da7eb9f6cc686d
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-11-05 10:57:45 -0600

    Implement hash functions

--- AppAcademy Open/4. Ruby/10. LRU Cache/2_hash_map/lib/p02_hashing.rb
+++ AppAcademy Open/4. Ruby/10. LRU Cache/2_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,13 @@ end
 
 class Array
   def hash
+    each_with_index.reduce(0) { |acc, (ele, idx)| acc ^ (ele.to_i + idx.hash).hash }
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +18,14 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = to_a.sort.flatten.map do |ele|
+      if ele.is_a?(Symbol)
+        ele.to_s.ord 
+      elsif ele.is_a?(String)
+        ele.ord
+      end
+    end
+
+    arr.hash
   end
 end
