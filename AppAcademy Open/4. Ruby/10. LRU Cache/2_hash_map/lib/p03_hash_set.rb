6ebfaf0d77a311a1d224da93449fe3ad099bb60e
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-11-05 11:06:41 -0600

    Implement HashSet

--- AppAcademy Open/4. Ruby/10. LRU Cache/2_hash_map/lib/p03_hash_set.rb
+++ AppAcademy Open/4. Ruby/10. LRU Cache/2_hash_map/lib/p03_hash_set.rb
@@ -7,18 +7,29 @@ class HashSet
   end
 
   def insert(key)
+    return if include?(key)
+
+    resize! if @count == num_buckets
+
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    return unless include?(key)
+
+    self[key].delete(key)
+    @count -= 1
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](key)
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +37,9 @@ class HashSet
   end
 
   def resize!
+    elements = @store.flatten
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+    elements.each { |ele| insert(ele) }
   end
 end
