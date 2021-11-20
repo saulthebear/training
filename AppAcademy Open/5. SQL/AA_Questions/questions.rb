d8ffefa27ded64e66e371f356efcf7a132044303
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-11-20 14:10:55 -0700

    Complete basic database mothods

--- AppAcademy Open/5. SQL/AA_Questions/questions.rb
+++ AppAcademy Open/5. SQL/AA_Questions/questions.rb
@@ -12,7 +12,7 @@ class QuestionsDatabase < SQLite3::Database
 end
 
 class User
-  attr_accessor :fname, :lname
+  attr_accessor :id, :fname, :lname
 
   def self.find_by_id(id)
     data = QuestionsDatabase.instance.execute(<<-SQL, id)
@@ -29,13 +29,21 @@ class User
         AND
         lname = ?
     SQL
-    Users.new(data.first)
+    User.new(data.first)
   end
   
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
+  end
+
+  def authored_questions
+    Question.find_by_author_id(@id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(@id)
   end
 end
 
@@ -49,11 +57,26 @@ class Question
     Question.new(data.first)
   end
 
+  def self.find_by_author_id(author_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+      SELECT * FROM questions WHERE author_id = ?
+    SQL
+    data.map { |datum| Question.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @author_id = options['author_id']
     @title = options['title']
     @body = options['body']
+  end
+
+  def author
+    User.find_by_id(@author_id)
+  end
+
+  def replies
+    Reply.find_by_question_id(@id)
   end
 end
 
@@ -83,12 +106,54 @@ class Reply
     Reply.new(data.first)
   end
   
+  def self.find_by_user_id(user_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT * FROM replies WHERE author_id = ?
+    SQL
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_question_id(question_id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT * FROM replies WHERE question_id = ?
+    SQL
+    data.map { |datum| Reply.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
     @parent_id = options['parent_id']
     @author_id = options['author_id']
     @body = options['body']
+  end
+
+  def author
+    User.find_by_id(@author_id)
+  end
+
+  def question
+    Question.find_by_id(@question_id)
+  end
+
+  def parent_reply
+    data = QuestionsDatabase.instance.execute(<<-SQL, @parent_id)
+      SELECT * FROM replies WHERE id = ?
+    SQL
+
+    return nil if data.length.zero?
+
+    Reply.new(data.first)
+  end
+
+  def child_replies
+    data = QuestionsDatabase.instance.execute(<<-SQL, @id)
+      SELECT * FROM replies WHERE parent_id = ?
+    SQL
+
+    return nil if data.length.zero?
+
+    data.map { |datum| Reply.new(datum) }
   end
 end
 
