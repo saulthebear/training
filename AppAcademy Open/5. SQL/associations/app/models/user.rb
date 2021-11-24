3014265b86e2f49453890d4dfe95b7b976a77277
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-11-23 18:09:21 -0700

    Complete Associations Project

--- AppAcademy Open/5. SQL/associations/app/models/user.rb
+++ AppAcademy Open/5. SQL/associations/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+    class_name: 'Enrollment',
+    foreign_key: :student_id
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
