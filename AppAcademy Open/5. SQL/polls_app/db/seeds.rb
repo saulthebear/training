69a5c34b3d56278ea78c7929b2490c582c849ad8
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-13 11:09:42 -0700

    Update database seed

--- AppAcademy Open/5. SQL/polls_app/db/seeds.rb
+++ AppAcademy Open/5. SQL/polls_app/db/seeds.rb
@@ -34,7 +34,8 @@ polls = Poll.create([
   { author: users[9], title: 'Poll 12' },
   { author: users[5], title: 'Poll 13' },
   { author: users[6], title: 'Poll 14' },
-  { author: users[7], title: 'Poll 15' }
+  { author: users[7], title: 'Poll 15' },
+  { author: users[0], title: 'Favorites' }
 ])
 
 questions = Question.create([
@@ -64,35 +65,57 @@ questions = Question.create([
   { poll: polls[7], text: 'Q1' },
   { poll: polls[8], text: 'Q2' },
   { poll: polls[8], text: 'Q3' },
-  { poll: polls[8], text: 'Q1' }
+  { poll: polls[8], text: 'Q1' },
+  { poll: polls[15], text: "Who's your favorite cat?"}
 ])
 
 answer_choices = AnswerChoice.create([
-  { question: questions[0], text: 'A'},
-  { question: questions[0], text: 'B'},
-  { question: questions[0], text: 'C'},
+  { question: questions[0], text: 'A' },
+  { question: questions[0], text: 'B' },
+  { question: questions[0], text: 'C' },
   
-  { question: questions[1], text: 'A'},
-  { question: questions[1], text: 'B'},
-  { question: questions[1], text: 'C'},
+  { question: questions[1], text: 'A' },
+  { question: questions[1], text: 'B' },
+  { question: questions[1], text: 'C' },
   
-  { question: questions[2], text: 'A'},
-  { question: questions[2], text: 'B'},
-  { question: questions[2], text: 'C'},
+  { question: questions[2], text: 'A' },
+  { question: questions[2], text: 'B' },
+  { question: questions[2], text: 'C' },
   
-  { question: questions[3], text: 'A'},
-  { question: questions[3], text: 'B'},
-  { question: questions[3], text: 'C'},
+  { question: questions[3], text: 'A' },
+  { question: questions[3], text: 'B' },
+  { question: questions[3], text: 'C' },
   
-  { question: questions[4], text: 'A'},
-  { question: questions[4], text: 'B'},
-  { question: questions[4], text: 'C'}
+  { question: questions[4], text: 'A' },
+  { question: questions[4], text: 'B' },
+  { question: questions[4], text: 'C' },
+
+  { question: questions[27], text: 'Earl' },
+  { question: questions[27], text: 'Breakfast' },
+  { question: questions[27], text: 'Maru' }
+
 ])
+
+earl = answer_choices[15]
+breakfast = answer_choices[16]
+maru = answer_choices[17]
 
 Response.create([
   { respondent: users[0], answer_choice: answer_choices[0] },
   { respondent: users[0], answer_choice: answer_choices[4] },
   { respondent: users[0], answer_choice: answer_choices[8] },
   { respondent: users[0], answer_choice: answer_choices[9] },
-  { respondent: users[0], answer_choice: answer_choices[13] }
+  { respondent: users[0], answer_choice: answer_choices[13] },
+
+  { respondent: User.find_by(username: 'Bodhi'), answer_choice: earl },
+  { respondent: User.find_by(username: 'George'), answer_choice: earl },
+  { respondent: User.find_by(username: 'Max'), answer_choice: earl },
+
+  { respondent: User.find_by(username: 'Kate'), answer_choice: breakfast },
+  { respondent: User.find_by(username: 'Jack'), answer_choice: breakfast },
+  { respondent: User.find_by(username: 'Sawyer'), answer_choice: breakfast },
+  { respondent: User.find_by(username: 'Hurley'), answer_choice: breakfast },
+
+  { respondent: User.find_by(username: 'Sayid'), answer_choice: maru },
+  { respondent: User.find_by(username: 'Boon'), answer_choice: maru }
 ])
