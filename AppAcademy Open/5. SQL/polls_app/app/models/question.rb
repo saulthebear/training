a549b28f4fff01dd45630053431f1c3ca4cc8cf0
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-13 11:30:57 -0700

    Optimize query for question results

--- AppAcademy Open/5. SQL/polls_app/app/models/question.rb
+++ AppAcademy Open/5. SQL/polls_app/app/models/question.rb
@@ -18,24 +18,29 @@ class Question < ApplicationRecord
     primary_key: :id,
     foreign_key: :poll_id
   )
-  
+
   has_many(
     :answer_choices,
     class_name: 'AnswerChoice',
     primary_key: :id,
     foreign_key: :question_id
   )
-  
+
   has_many(
     :responses,
     through: :answer_choices,
     source: :responses
   )
-  
+
   def results
+    answer_choices_with_responses = answer_choices
+                                    .select('answer_choices.*, COUNT(*) AS response_count')
+                                    .joins(:responses)
+                                    .group('answer_choices.id')
+
     results_hash = {}
-    answer_choices.includes(:responses).each do |choice|
-      results_hash[choice.text] = choice.responses.length
+    answer_choices_with_responses.each do |ac|
+      results_hash[ac.text] = ac.response_count
     end
     results_hash
   end
