5847fc4d6e162680fc1f40ce76e21278377c4583
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-14 11:59:29 -0700

    Enable database searching through the model

--- AppAcademy Open/5. SQL/ActiveRecordLite/lib/02_searchable.rb
+++ AppAcademy Open/5. SQL/ActiveRecordLite/lib/02_searchable.rb
@@ -3,10 +3,23 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params
+      .map { |attr, _| "#{attr} = ?" }
+      .join(' AND ')
+    query = <<-SQL
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+    results = DBConnection.execute(query, *params.values)
+    parse_all(results)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
