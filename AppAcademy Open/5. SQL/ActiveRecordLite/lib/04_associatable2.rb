0fae6110542d7f0a793c5d92e1fd32ca6ca52cb3
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-14 16:46:23 -0700

    Enable has_one_through association

--- AppAcademy Open/5. SQL/ActiveRecordLite/lib/04_associatable2.rb
+++ AppAcademy Open/5. SQL/ActiveRecordLite/lib/04_associatable2.rb
@@ -5,6 +5,22 @@ module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      query = <<-SQL
+        SELECT #{source_options.table_name}.*
+        FROM #{through_options.table_name}
+        JOIN #{source_options.table_name}
+          ON #{through_options.table_name}.#{source_options.foreign_key} = #{source_options.table_name}.#{source_options.primary_key}
+        WHERE #{through_options.table_name}.#{through_options.primary_key} = ?
+      SQL
+
+      result = DBConnection.execute(query, send(through_options.foreign_key)).first
+      return nil if result.nil?
+
+      source_options.model_class.new(result)
+    end
   end
 end
