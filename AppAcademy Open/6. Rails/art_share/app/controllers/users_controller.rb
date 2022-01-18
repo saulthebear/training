963f1ad3f6204a08217ca475c4c6f7ec8cf38697
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-18 08:03:42 -0700

    Add User controller

--- AppAcademy Open/6. Rails/art_share/app/controllers/users_controller.rb
+++ AppAcademy Open/6. Rails/art_share/app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+  def index
+    # /users GET
+    render json: User.all
+  end
+  
+  def create
+    # /users POST
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entitiy
+    end
+  end
+
+  # def new
+  #   # /users/new GET
+  # end
+
+  # def edit
+  #   # /users/:id/edit GET
+  # end
+
+  def show
+    # /users/:id GET
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    # /users/:id PATCH PUT
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entitiy
+    end
+  end
+
+  def destroy
+    # /users/:id DELETE
+    User.destroy(params[:id])
+    render plain: "User #{params[:id]} was destroyed!"
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
