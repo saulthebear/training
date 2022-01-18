5f6ee7545844cc97a99bbff6934f589259f63742
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-18 09:18:24 -0700

    Limit routes
    Edit and New are not needed since this is an API only

--- AppAcademy Open/6. Rails/art_share/config/routes.rb
+++ AppAcademy Open/6. Rails/art_share/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artworks
-  resources :users
+  resources :artworks, only: %i[index create destroy show update]
+  resources :users, only: %i[index create destroy show update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
