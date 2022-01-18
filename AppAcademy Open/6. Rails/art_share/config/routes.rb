02c74b15f509d3f8528766f75b722a42007c38d5
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-18 10:03:47 -0700

    Enable artwork sharing through ArtworkShares endpoints An artwork is shared by creating an ArtworkShare record

--- AppAcademy Open/6. Rails/art_share/config/routes.rb
+++ AppAcademy Open/6. Rails/art_share/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :artworks, only: %i[index create destroy show update]
-  resources :users, only: %i[index create destroy show update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index create destroy show update]
+  resources :artworks, only: %i[index create destroy show update]
+  resources :artwork_shares, only: %i[create destroy]
 end
