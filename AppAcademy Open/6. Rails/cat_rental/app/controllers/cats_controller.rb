f755dfd0d1ef2ad5045d7fee51122f262f8e6d96
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-21 18:18:36 -0700

    Add Cat index and show actions and views

--- AppAcademy Open/6. Rails/cat_rental/app/controllers/cats_controller.rb
+++ AppAcademy Open/6. Rails/cat_rental/app/controllers/cats_controller.rb
@@ -1,8 +1,12 @@
 class CatsController < ApplicationController
   def index
+    @cats = Cat.all
+    render :index
   end
 
   def show
+    @cat = Cat.find_by(id: params[:id])
+    render :show
   end
 
   def new
