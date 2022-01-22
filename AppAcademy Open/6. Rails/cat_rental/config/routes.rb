014ffa823e2bb16721a0cd673362377a91637c10
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-21 18:16:48 -0700

    Add Cat routes

--- AppAcademy Open/6. Rails/cat_rental/config/routes.rb
+++ AppAcademy Open/6. Rails/cat_rental/config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  resources :cats, only: %i[index show new update create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
