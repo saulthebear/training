b5c637b274bfd3f1f6ac7a4edf2af6b3894845ed
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-22 10:56:25 -0700

    Add "new" action and view for RentalRequests

---
+++ AppAcademy Open/6. Rails/cat_rental/app/controllers/cat_rental_requests_controller.rb
@@ -0,0 +1,10 @@
+class CatRentalRequestsController < ApplicationController
+  def new
+    @cats = Cat.all
+    render :new
+  end
+
+  def create
+    render json: params
+  end
+end
--- AppAcademy Open/6. Rails/cat_rental/config/routes.rb
+++ AppAcademy Open/6. Rails/cat_rental/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :cats, only: %i[index show new update create edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cats, only: %i[index show new update create edit]
+  resources :cat_rental_requests, only: %i[new create]
 end
