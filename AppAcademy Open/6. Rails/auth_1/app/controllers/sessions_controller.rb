aab4fb9e713e9aaaaa7b503a1a00191e949c5bd9
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-24 12:02:04 -0700

    Allow login through session tokens

--- AppAcademy Open/6. Rails/auth_1/app/controllers/sessions_controller.rb
+++ AppAcademy Open/6. Rails/auth_1/app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
     if user.nil?
       render json: 'Credentials were wrong'
     else
-      render json: "Welcome back, #{user.username}!"
+      login!(user)
+      redirect_to user_url(user)
     end
   end
 end
