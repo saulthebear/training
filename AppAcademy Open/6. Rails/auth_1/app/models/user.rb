008d37e670654235ad35f1330b271d14c1235aa9
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-23 20:30:32 -0700

    Add password methods on user model

--- AppAcademy Open/6. Rails/auth_1/app/models/user.rb
+++ AppAcademy Open/6. Rails/auth_1/app/models/user.rb
@@ -9,4 +9,13 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  validates :username, presence: true
+  
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
