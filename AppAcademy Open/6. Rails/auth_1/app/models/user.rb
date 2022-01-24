088208ec12eba93d1b83f81cdfd00d3bde348675
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-24 11:33:39 -0700

    Add method to find users by login credentials

--- AppAcademy Open/6. Rails/auth_1/app/models/user.rb
+++ AppAcademy Open/6. Rails/auth_1/app/models/user.rb
@@ -15,6 +15,13 @@ class User < ApplicationRecord
   validates :password_digest, presence: { message: 'Password cant\'t be blank'}
   validates :password, length: { minimum: 6, allow_nil: true }
   
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+
+    user.is_password?(password) ? user : nil
+  end
+  
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
