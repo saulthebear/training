c442660377655cf0618b71ad96d1eaa387ed6f3c
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-25 16:51:39 -0700

    Add current_user helper method for all controllers

--- AppAcademy Open/6. Rails/cat_rental/app/controllers/application_controller.rb
+++ AppAcademy Open/6. Rails/cat_rental/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  
+  def current_user
+    session_token = session[:session_token]
+    return nil unless session_token
+
+    @current_user ||= User.find_by(session_token: session_token)
+  end
 end
