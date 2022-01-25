2a7a522600629256fc8bfa2d706e6efb61136b73
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-25 12:24:55 -0700

    Add session create action

--- AppAcademy Open/6. Rails/cat_rental/app/controllers/sessions_controller.rb
+++ AppAcademy Open/6. Rails/cat_rental/app/controllers/sessions_controller.rb
@@ -2,4 +2,24 @@ class SessionsController < ApplicationController
   def new
     render :new
   end
+  
+  def create
+    user = User.find_by_credentials(
+      session_params[:username],
+      session_params[:password]
+    )
+    if user
+      user.reset_session_token!
+      redirect_to cats_url
+    else
+      flash.now[:errors] = ["Invalid login details"]
+      render :new
+    end
+  end
+  
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :password)
+  end
 end
