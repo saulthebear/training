17df81a30b5e830fdfadefa5c628c390304bcf3e
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-24 20:38:09 -0700

    Add user model

--- AppAcademy Open/6. Rails/cat_rental/app/models/user.rb
+++ AppAcademy Open/6. Rails/cat_rental/app/models/user.rb
@@ -10,4 +10,44 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  attr_reader :password
+  
+  validates :username, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by(username: username)
+    return user if user&.is_password?(password)
+
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+  
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    return self.session_token
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+  
+  def is_password?(password)
+    self.password_digest.is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
 end
