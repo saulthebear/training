f7fc3eff4a9369ff930140e785256b81e0512685
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-25 16:51:20 -0700

    Add session destroy action to logout user

--- AppAcademy Open/6. Rails/cat_rental/app/controllers/sessions_controller.rb
+++ AppAcademy Open/6. Rails/cat_rental/app/controllers/sessions_controller.rb
@@ -9,12 +9,22 @@ class SessionsController < ApplicationController
       session_params[:password]
     )
     if user
-      user.reset_session_token!
+      session_token = user.reset_session_token!
+      session[:session_token] = session_token
       redirect_to cats_url
     else
       flash.now[:errors] = ["Invalid login details"]
       render :new
     end
+  end
+  
+  def destroy
+    if current_user
+      current_user.reset_session_token!
+      session[:session_token] = nil
+    end
+
+    redirect_to new_session_url
   end
   
   private
