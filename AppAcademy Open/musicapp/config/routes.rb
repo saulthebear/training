a90d374d26e546f65c4f490464f5ab2c68e1b71a
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-29 09:00:46 -0700

    Add album table model and routes

--- AppAcademy Open/musicapp/config/routes.rb
+++ AppAcademy Open/musicapp/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[new create index]
   resource :session, only: %i[new create destroy]
-  resources :bands
+  resources :bands do
+    resources :albums, only: %i[new]
+  end
+  resources :albums, except: %i[new index]
 end
--- AppAcademy Open/musicapp/db/schema.rb
+++ AppAcademy Open/musicapp/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_024347) do
+ActiveRecord::Schema.define(version: 2022_01_29_154235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "year", null: false
+    t.boolean "is_live", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "band_id"
+    t.index ["band_id"], name: "index_albums_on_band_id"
+  end
 
   create_table "bands", force: :cascade do |t|
     t.string "name", null: false
