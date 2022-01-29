23a77050a20dd244f8ae4a2de0743a378fe562c0
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-29 12:31:06 -0700

    Add create and edit actions for albums

--- AppAcademy Open/musicapp/app/controllers/albums_controller.rb
+++ AppAcademy Open/musicapp/app/controllers/albums_controller.rb
@@ -7,4 +7,38 @@ class AlbumsController < ApplicationController
       redirect_to '/'
     end
   end
+  
+  def new
+    @album = Album.new
+    @bands = Band.all
+    render :new
+  end
+
+  def edit
+    @album = Album.includes(:band).find_by(id: params[:id])
+    @bands = Band.all
+    if @album
+      render :edit
+    else
+      redirect_to '/'
+    end
+  end
+  
+  def create
+    album = Album.new(album_params)
+    if album.save
+      redirect_to album_url(album.id)
+    else
+      flash.now[:erors] = album.errors.full_messages
+      @album = album
+      @bands = Band.all
+      render :new
+    end
+  end
+  
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :band_id, :year, :is_live)
+  end
 end
