9d1d8fa9e88e8048cfdb126089c60b4f54358e6f
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-01-28 20:38:43 -0700

    Add bands resource

--- AppAcademy Open/musicapp/db/schema.rb
+++ AppAcademy Open/musicapp/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_210454) do
+ActiveRecord::Schema.define(version: 2022_01_29_024347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
