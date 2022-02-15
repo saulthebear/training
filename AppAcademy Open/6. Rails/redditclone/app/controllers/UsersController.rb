5aad0fb694690194a582ebaff5964c3206643b86
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-02-14 17:00:24 -0700

    Ensure user is logged in as soon as they sign up

--- AppAcademy Open/6. Rails/redditclone/app/controllers/UsersController.rb
+++ AppAcademy Open/6. Rails/redditclone/app/controllers/UsersController.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      login!(@user)
       redirect_to :root
     else
       flash.now[:errors] = @user.errors.full_messages
