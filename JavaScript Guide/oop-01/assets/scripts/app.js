f10af3ed32b67a7d9892e6da65eef6352ceef93b
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-03 11:59:29 -0700

    Render product list

---
+++ JavaScript Guide/oop-01/assets/scripts/app.js
@@ -0,0 +1,44 @@
+const ProductList = {
+  products: [
+    {
+      title: "A pillow",
+      imageUrl:
+        "https://target.scene7.com/is/image/Target/GUEST_3d52ad7a-40b3-4a3f-ac4f-e10b19b17ccc?wid=488&hei=488&fmt=pjpeg",
+      price: 19.99,
+      description: "A soft pillow!",
+    },
+    {
+      title: "A carpet",
+      imageUrl:
+        "https://media.istockphoto.com/photos/turkish-carpet-picture-id486167735?k=20&m=486167735&s=612x612&w=0&h=C-hW6Mj4YpUwCLl9hp_n8IVqxf-By0X1RmT_xgsl3d8=",
+      price: 99.99,
+      description: "What a great pillow!",
+    },
+  ],
+
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    /* eslint-disable no-restricted-syntax */
+    for (const product of this.products) {
+      const prodEl = document.createElement("li");
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+        <div>
+          <img src="${product.imageUrl}" alt="${product.description}">
+          <div class="product-item__content">
+            <h2>${product.title}</h2>
+            <h3>$${product.price}</h3>
+            <p>${product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  },
+};
+
+ProductList.render();
