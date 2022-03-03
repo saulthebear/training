12d08cb52522243b0177ee413556cc7f92ae4f19
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-03 16:12:37 -0700

    Complete problem 1

---
+++ JavaScript Guide/oop-assignment/assignment.js
@@ -0,0 +1,19 @@
+/*
+  Create a "Course" class which has three properties: "title", "length" and
+  "price" (all set up in the constructor). Instantiate the class twice (two
+  dummy courses of your choice). Output the objects to the console
+*/
+
+class Course {
+  constructor(title, length, price) {
+    this.title = title;
+    this.length = length;
+    this.price = price;
+  }
+}
+
+const course1 = new Course("JS Complete Guide", 10, 11);
+const course2 = new Course("React", 5, 50);
+
+console.log(course1);
+console.log(course2);
