e294664886d058667b49fff948d94e30ff1b6bcd
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-02 18:33:35 -0700

    Display added movies

--- JavaScript Guide/objects-07-rendering-movies/assets/scripts/objects.js
+++ JavaScript Guide/objects-07-rendering-movies/assets/scripts/objects.js
@@ -3,6 +3,32 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = () => {
+  const movieList = document.getElementById("movie-list");
+
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  }
+
+  movieList.classList.add("visible");
+
+  movieList.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement("li");
+    let text = `${movie.info.title} -- `;
+    /* eslint-disable no-restricted-syntax */
+    for (const key in movie.info) {
+      if (key !== "title") {
+        text += `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieElement.textContent = text;
+    movieList.append(movieElement);
+  });
+};
+
 const addMovieHandler = () => {
   const title = document.getElementById("title").value;
   const extraName = document.getElementById("extra-name").value;
@@ -19,6 +45,7 @@ const addMovieHandler = () => {
   };
 
   movies.push(newMovie);
+  renderMovies();
 };
 
 addMovieBtn.addEventListener("click", addMovieHandler);
