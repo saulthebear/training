e6f7f0bab3653d78987b3cf0830f93014a5c6a27
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-03 10:02:28 -0700

    Add title filter

--- JavaScript Guide/objects-07-rendering-movies/assets/scripts/objects.js
+++ JavaScript Guide/objects-07-rendering-movies/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = "") => {
   const movieList = document.getElementById("movie-list");
 
   if (movies.length === 0) {
@@ -15,7 +15,11 @@ const renderMovies = () => {
 
   movieList.innerHTML = "";
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
     const movieElement = document.createElement("li");
     let text = `${movie.info.title} -- `;
     /* eslint-disable no-restricted-syntax */
@@ -48,4 +52,10 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
