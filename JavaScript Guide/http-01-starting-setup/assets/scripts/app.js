aedc22ce128a59d6cc9cc4d95a0afa2b65104441
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-04 18:11:31 -0700

    Send HTTP request on form submit

--- JavaScript Guide/http-01-starting-setup/assets/scripts/app.js
+++ JavaScript Guide/http-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,9 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
@@ -10,7 +11,7 @@ function sendHttpRequest(method, url) {
 
     xhr.onload = () => resolve(xhr.response);
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
 
   return promise;
@@ -35,4 +36,22 @@ async function fetchPosts() {
   }
 }
 
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title,
+    body: content,
+    userId,
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
 fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredContent);
+});
