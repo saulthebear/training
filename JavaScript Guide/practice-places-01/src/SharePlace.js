7c34120be776088fb4b295f5e56c7335e2cf83a3
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-06 08:15:39 -0700

    Show spinner modal while getting user location

--- JavaScript Guide/practice-places-01/src/SharePlace.js
+++ JavaScript Guide/practice-places-01/src/SharePlace.js
@@ -1,3 +1,5 @@
+import Modal from "./UI/Modal";
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector("form");
@@ -8,8 +10,11 @@ class PlaceFinder {
   }
 
   #locateUserHandler() {
+    const modal = new Modal("loading-modal-content", "Loading location...");
+    modal.show();
     navigator.geolocation.getCurrentPosition(
       (successResult) => {
+        modal.hide();
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude,
@@ -17,6 +22,7 @@ class PlaceFinder {
         console.log(coordinates);
       },
       (error) => {
+        modal.hide();
         alert("Courld not locate you. Please enter an address manually.");
       }
     );
