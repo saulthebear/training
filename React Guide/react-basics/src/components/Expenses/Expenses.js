e6a784a351276c14ceae15d54655d8b019a12cdf
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-08 17:08:18 -0700

    Render ExpenseItems as array

--- React Guide/react-basics/src/components/Expenses/Expenses.js
+++ React Guide/react-basics/src/components/Expenses/Expenses.js
@@ -22,26 +22,15 @@ function Expenses(props) {
         onFilterChange={filterChangeHandler}
         selectedYear={filterYear}
       />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+
+      {expenses.map((expense) => (
+        <ExpenseItem
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 }
