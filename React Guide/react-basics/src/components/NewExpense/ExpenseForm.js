bd04af44bb9163b8a767a542454d24ca01fadacc
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-09 10:32:52 -0700

    Bug fix: cast amount to number on form submit

--- React Guide/react-basics/src/components/NewExpense/ExpenseForm.js
+++ React Guide/react-basics/src/components/NewExpense/ExpenseForm.js
@@ -27,7 +27,7 @@ function ExpenseForm(props) {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
