09bae3eb0576428fcb1c640d209df9d55a445145
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-10 15:39:25 -0700

    Add two way binding to new user form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Button from './UI/Button';
 import Card from './UI/Card';
 
@@ -5,18 +7,31 @@ function Form(props) {
   const ERROR_AGE = 'ERROR_AGE';
   const ERROR_GENERAL = 'ERROR_GENERAL';
 
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const resetInputs = () => {
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const form = event.target;
 
-    const usernameInput = form.querySelector('#username').value.trim();
-    const ageInput = form.querySelector('#age').value.trim();
-
-    const validUsername = usernameInput !== '';
-    const validAge = ageInput && +ageInput > 0;
+    const validUsername = enteredUsername !== '';
+    const validAge = enteredAge && +enteredAge > 0;
 
     if (validUsername && validAge) {
-      props.onSubmit({ username: usernameInput, age: +ageInput });
+      props.onSubmit({ username: enteredUsername, age: +enteredAge });
+      resetInputs();
       return;
     }
 
@@ -42,6 +57,8 @@ function Form(props) {
           className="mb-3 rounded border-2 p-1 px-3"
           type="text"
           id="username"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
         />
 
         <label className="mb-1 font-bold" htmlFor="age">
@@ -53,6 +70,8 @@ function Form(props) {
           id="age"
           min="0"
           step="1"
+          onChange={ageChangeHandler}
+          value={enteredAge}
         />
         <Button type="submit">Add User</Button>
       </form>
