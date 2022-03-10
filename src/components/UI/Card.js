506b1eb328cb299f9458729ea8c46a486cd59059
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-10 10:57:45 -0700

    Add header to card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,13 @@
 function Card(props) {
   return (
-    <div className="rounded-lg bg-slate-100 px-4 py-5">{props.children}</div>
+    <div className="overflow-scroll rounded-lg">
+      {props.header && (
+        <div className="bg-purple-700 px-4 py-5 font-bold text-white">
+          {props.header}
+        </div>
+      )}
+      <div className="bg-slate-100 px-4 py-5">{props.children}</div>
+    </div>
   );
 }
 
