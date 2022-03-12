d15ad3c794a92b1054994c414d970cd297f98c7e
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-12 09:46:58 -0700

    Refactor CartButton

--- React Guide/food-order/src/Components/Cart/CartButton.js
+++ React Guide/food-order/src/Components/Cart/CartButton.js
@@ -3,10 +3,10 @@ import CartIcon from '../Icons/CartIcon';
 function Cart(props) {
   return (
     <button className="flex  items-center rounded-full bg-orange-900 px-8  py-3 font-bold">
-      <span>
-        <CartIcon />
-      </span>
+      <CartIcon />
+
       <span className="mx-3">Your Cart</span>
+
       <span className="rounded-full bg-orange-700 px-3 py-1">
         {props.itemCount}
       </span>
