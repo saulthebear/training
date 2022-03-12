95a8f94c3d6d510de8322c177c0defe3d8b8f21d
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-12 13:01:09 -0700

    Add cart context

---
+++ React Guide/food-order/src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import CartContext from './cart-context';
+
+function CartProvider(props) {
+  const addItemToCartHandler = (item) => {};
+  const removeItemFromCartHandler = (itemId) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
+
+export default CartProvider;
