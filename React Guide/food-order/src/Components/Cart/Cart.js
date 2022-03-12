8c660123d0a3e79442d60ed2fa800e180647594b
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-12 10:01:04 -0700

    Fix invalid prop name
    Change "class" to "className"

--- React Guide/food-order/src/Components/Cart/Cart.js
+++ React Guide/food-order/src/Components/Cart/Cart.js
@@ -30,16 +30,20 @@ function Cart(props) {
         />
       ))}
 
-      <div class="mb-6 flex justify-between border-t-2 border-orange-900 pt-3 text-3xl font-bold">
+      <div className="mb-6 flex justify-between border-t-2 border-orange-900 pt-3 text-3xl font-bold">
         <div>Total Amount</div>
         <div>$33.00</div>
       </div>
 
-      <div class="flex justify-end text-xl">
-        <button className="mr-5 rounded-full border-2 border-orange-900 px-8 py-1 text-orange-900 shadow-orange-900 last:mr-0">
+      <div className="flex justify-end text-xl">
+        <button
+          className="mr-5 rounded-full border-2 border-orange-900 px-8 py-1 text-orange-900 shadow-orange-900 last:mr-0"
+        >
           Close
         </button>
-        <button className="mr-5 rounded-full bg-orange-900 px-8 py-1 text-white shadow-orange-900 last:mr-0">
+        <button
+          className="mr-5 rounded-full bg-orange-900 px-8 py-1 text-white shadow-orange-900 last:mr-0"
+        >
           Order
         </button>
       </div>
--- React Guide/food-order/src/Components/Cart/CartItem.js
+++ React Guide/food-order/src/Components/Cart/CartItem.js
@@ -3,11 +3,11 @@ function CartItem(props) {
     <div className="mb-6 flex w-full items-center justify-between">
       <div className="flex flex-col">
         <div className="mb-1 text-2xl font-bold">{props.name}</div>
-        <div class="flex justify-between">
-          <div class="mr-10 font-semibold text-orange-900">
+        <div className="flex justify-between">
+          <div className="mr-10 font-semibold text-orange-900">
             ${(props.unitPrice / 100).toFixed(2)}
           </div>
-          <div class="rounded-md  border-2 px-3 py-1 text-sm font-bold">
+          <div className="rounded-md  border-2 px-3 py-1 text-sm font-bold">
             x{props.amount}
           </div>
         </div>
