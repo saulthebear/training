f06c4fbf3074c78bc539ee708685bbdd9381e7bf
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-12 15:24:28 -0700

    Add cart state and updating functions

--- React Guide/food-order/src/store/CartProvider.js
+++ React Guide/food-order/src/store/CartProvider.js
@@ -1,12 +1,37 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.unitPrice * action.item.quantity;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  return defaultCartState;
+};
+
 function CartProvider(props) {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (itemId) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD', item });
+  };
+  const removeItemFromCartHandler = (itemId) => {
+    dispatchCartAction({ type: 'REMOVE', itemId });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
