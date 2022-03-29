1c387d8b6f1b5fdde20906f30d02b0adef267f29
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-03-29 15:41:09 -0600

    Show loading and error states

--- React Guide/food-order/src/Components/Menu/Menu.js
+++ React Guide/food-order/src/Components/Menu/Menu.js
@@ -5,12 +5,20 @@ import MenuItem from './MenuItem';
 
 function Menu() {
   const [menu, setMenu] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         'https://reactguide-49a53-default-rtdb.firebaseio.com/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -24,10 +32,30 @@ function Menu() {
       }
 
       setMenu(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <Card className="mx-auto w-[90%] max-w-5xl">
+        <p className="text-center">Loading...</p>
+      </Card>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <Card className="mx-auto w-[90%] max-w-5xl">
+        <p className="text-center">{httpError}</p>
+      </Card>
+    );
+  }
 
   const menuItems = menu.map((item) => (
     <li key={item.id} className="pb-5 last:pb-0">
