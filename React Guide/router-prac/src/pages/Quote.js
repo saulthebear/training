9cb7463c6d6729ad163c5ac135f65946a513a66c
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-04-01 14:11:41 -0600

    Get and store quotes from backend

--- React Guide/router-prac/src/pages/Quote.js
+++ React Guide/router-prac/src/pages/Quote.js
@@ -1,34 +1,46 @@
-import { Link } from 'react-router-dom';
-import { useParams, Route, useRouteMatch } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, Route, useRouteMatch, Link } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
-
-const DUMMY_QUOTES = [
-  {
-    id: 'q1',
-    author: 'Max',
-    text: 'Learning React is fun!',
-  },
-  {
-    id: 'q2',
-    author: 'Maximilian',
-    text: 'Learning React is great!',
-  },
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
 
 function Quote() {
   const match = useRouteMatch();
   const params = useParams();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if (!quote) {
+  const {
+    sendRequest,
+    status,
+    error,
+    data: loadedQuote,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(params.quoteId);
+  }, [sendRequest, params.quoteId]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>Quote not found</p>;
   }
 
   return (
     <section>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
