b34ec23407a863a02c827c5c9e0fa10bd789e770
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-04-01 11:16:07 -0600

    Add redirect route

--- React Guide/router/src/App.js
+++ React Guide/router/src/App.js
@@ -1,3 +1,4 @@
+import { Redirect } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 
 import MainHeader from './components/MainHeader';
@@ -10,6 +11,9 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
