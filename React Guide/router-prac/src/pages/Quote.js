a705ac4ffa6f6aa1ad1304c94d719a70f9575eab
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-04-01 12:12:49 -0600

    Display quote detail

--- React Guide/router-prac/src/pages/Quote.js
+++ React Guide/router-prac/src/pages/Quote.js
@@ -1,13 +1,32 @@
 import { useParams, Route } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'Learning React is fun!',
+  },
+  {
+    id: 'q2',
+    author: 'Maximilian',
+    text: 'Learning React is great!',
+  },
+];
 
 function Quote() {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>Quote not found</p>;
+  }
+
   return (
     <section>
-      <h1>Quote details...</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
