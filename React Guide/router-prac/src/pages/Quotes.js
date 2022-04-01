65a9ef71a5f9a2706acf31950ba68ee3e15c8155
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-04-01 12:02:45 -0600

    Show all quotes and new quote form

--- React Guide/router-prac/src/pages/Quotes.js
+++ React Guide/router-prac/src/pages/Quotes.js
@@ -1,5 +1,19 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Max',
+    text: 'Learning React is fun!',
+  },
+  {
+    id: 'q2',
+    author: 'Maximilian',
+    text: 'Learning React is great!',
+  },
+];
 function Quotes() {
-  return <h1>The Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
 
 export default Quotes;
